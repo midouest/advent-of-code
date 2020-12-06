f702e5d437da40376b29567331f42cddadd8ac2e
Robin <midouesterner@gmail.com>
2020-12-06 14:38:20 -0600

    add split_groups helper

--- src/core/util.rs
+++ src/core/util.rs
@@ -7,3 +7,58 @@ pub fn last_n(n: usize, max: usize) -> Range<usize> {
         n - max..n
     }
 }
+
+pub fn split_groups(contents: &str) -> Vec<Vec<&str>> {
+    let mut groups = Vec::new();
+    let mut buffer = Vec::new();
+
+    for line in contents.lines() {
+        if line.is_empty() {
+            groups.push(buffer);
+            buffer = Vec::new();
+        } else {
+            buffer.push(line);
+        }
+    }
+    groups.push(buffer);
+    groups
+}
+
+#[cfg(test)]
+mod tests {
+    use super::split_groups;
+
+    #[test]
+    fn it_splits_groups() {
+        let contents = "foo
+bar
+baz
+
+qux
+quux
+quuz
+
+corge
+grault
+garply
+
+waldo
+fred
+plugh
+xyzzy
+
+thud";
+
+        let groups = split_groups(contents);
+        assert_eq!(
+            groups,
+            vec![
+                vec!["foo", "bar", "baz"],
+                vec!["qux", "quux", "quuz"],
+                vec!["corge", "grault", "garply"],
+                vec!["waldo", "fred", "plugh", "xyzzy"],
+                vec!["thud"]
+            ]
+        );
+    }
+}
